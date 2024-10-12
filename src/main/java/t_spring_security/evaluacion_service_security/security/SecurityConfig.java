5a48f666d7164b635e1c8275c4a17730c71ea2c3
zchanway <146023170+zchanway@users.noreply.github.com>
2024-10-12 04:12:20 -0500

    security filter

--- src/main/java/t_spring_security/evaluacion_service_security/security/SecurityConfig.java
+++ src/main/java/t_spring_security/evaluacion_service_security/security/SecurityConfig.java
@@ -7,8 +7,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import t_spring_security.evaluacion_service_security.service.DetalleUsuarioService;
 
 @EnableMethodSecurity
@@ -33,4 +36,22 @@ public class SecurityConfig {
             AuthenticationConfiguration configuration) throws Exception{
         return configuration.getAuthenticationManager();
     }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/login").permitAll() // Permitir acceso a la autenticación
+                        .requestMatchers("/api/gestor/**").hasRole("GESTOR") // Permitir solo a GESTOR
+                        .requestMatchers("/api/coordinador/**").hasRole("COORDINADOR") // Permitir solo a COORDINADOR
+                        .requestMatchers("/api/shared/**").hasAnyRole("GESTOR", "COORDINADOR") // Permitir a ambos roles
+                        .anyRequest().authenticated() // Proteger todas las demás rutas
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // No usar sesiones
+
+        // Desactivar CSRF si es necesario
+        http.csrf(csrf -> csrf.disable()); // Desactivar CSRF para aplicaciones RESTful
+
+        return http.build();
+    }
 }
+

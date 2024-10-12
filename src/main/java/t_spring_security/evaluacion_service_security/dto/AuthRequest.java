7268b60ad426d25e59ae79f0ac983093a9cca4ed
Melys02 <melys0919@gmail.com>
2024-10-12 03:43:08 -0500

    request listo

--- src/main/java/t_spring_security/evaluacion_service_security/dto/AuthRequest.java
+++ src/main/java/t_spring_security/evaluacion_service_security/dto/AuthRequest.java
@@ -1,4 +1,12 @@
 package t_spring_security.evaluacion_service_security.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 public class AuthRequest {
+    private String codigo;
+    private String password;
 }

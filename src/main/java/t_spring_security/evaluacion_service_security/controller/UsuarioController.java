50ddc2f19848580422ec60223e53c0cc855430a6
Melys02 <melys0919@gmail.com>
2024-10-12 03:42:15 -0500

    usuario request

--- src/main/java/t_spring_security/evaluacion_service_security/controller/UsuarioController.java
+++ src/main/java/t_spring_security/evaluacion_service_security/controller/UsuarioController.java
@@ -1,4 +1,25 @@
 package t_spring_security.evaluacion_service_security.controller;
 
-public class UsuarioController {
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import t_spring_security.evaluacion_service_security.dto.UsuarioRequest;
+import t_spring_security.evaluacion_service_security.model.Usuario;
+import t_spring_security.evaluacion_service_security.service.UsuarioService;
+
+public class UsuarioController
+{
+    private UsuarioService usuarioService;
+
+    // API para registrar un usuario con un rol
+    @PostMapping("/registrar")
+    public String registrarUsuario(@RequestBody UsuarioRequest usuarioRequest) {
+        Usuario usuario = usuarioService.registrarUsuario(
+                usuarioRequest.getCodigo(),
+                usuarioRequest.getPassword(),
+                usuarioRequest.getEmail(),
+                usuarioRequest.getRol()
+        );
+        return "Usuario " + usuario.getCodigo() + " registrado exitosamente con rol " + usuario.getRol();
+    }
 }
+
--- src/main/java/t_spring_security/evaluacion_service_security/dto/UsuarioRequest.java
+++ src/main/java/t_spring_security/evaluacion_service_security/dto/UsuarioRequest.java
@@ -1,4 +1,15 @@
 package t_spring_security.evaluacion_service_security.dto;
 
-public class UsuarioRequest {
+import lombok.Getter;
+import lombok.Setter;
+import t_spring_security.evaluacion_service_security.model.Usuario;
+
+    @Getter
+    @Setter
+    public class UsuarioRequest {
+
+        private String codigo;
+        private String password;
+        private String email;
+        private Usuario.Rol rol;
 }

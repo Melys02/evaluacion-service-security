19c6edcbddbf1332697ea11e07c8904dcc1e2191
Melys02 <melys0919@gmail.com>
2024-10-12 03:45:10 -0500

    detailuser

--- src/main/java/t_spring_security/evaluacion_service_security/service/DetalleUsuarioService.java
+++ src/main/java/t_spring_security/evaluacion_service_security/service/DetalleUsuarioService.java
@@ -1,4 +1,29 @@
 package t_spring_security.evaluacion_service_security.service;
 
-public class DetalleUsuarioService {
-}
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import t_spring_security.evaluacion_service_security.model.Usuario;
+import t_spring_security.evaluacion_service_security.repository.UsuarioRepository;
+
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Service
+public class DetalleUsuarioService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String codigo) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByCodigo(codigo)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + codigo));
+
+        return new org.springframework.security.core.userdetails.User(
+                usuario.getCodigo(), usuario.getPassword(), Collections.emptyList());
+
+    }  }

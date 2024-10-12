8c2b6060594ab70c6b6c406de7b88a9bc2085f67
Melys02 <melys0919@gmail.com>
2024-10-12 03:26:37 -0500

    inicio de proyecto

---
+++ src/main/java/t_spring_security/evaluacion_service_security/EvaluacionServiceSecurityApplication.java
@@ -0,0 +1,13 @@
+package t_spring_security.evaluacion_service_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionServiceSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacionServiceSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/t_spring_security/evaluacion_service_security/EvaluacionServiceSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package t_spring_security.evaluacion_service_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluacionServiceSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

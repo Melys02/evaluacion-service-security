3b9642ea724a37bcc4a8955a6b4c2aeb240f6581
Melys02 <melys0919@gmail.com>
2024-10-12 03:35:52 -0500

    repositorio terminado

--- src/main/java/t_spring_security/evaluacion_service_security/repository/UsuarioRepository.java
+++ src/main/java/t_spring_security/evaluacion_service_security/repository/UsuarioRepository.java
@@ -1,4 +1,17 @@
 package t_spring_security.evaluacion_service_security.repository;
 
-public interface UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import t_spring_security.evaluacion_service_security.model.Usuario;
+
+import java.util.Optional;
+
+@Repository
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByCodigo(String codigo);
+
+    Usuario registrarUsuario(String codigo, String password, String email, Usuario.Rol rol);
+
 }

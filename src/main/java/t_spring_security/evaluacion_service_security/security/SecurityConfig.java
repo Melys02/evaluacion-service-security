b312dd147581b0489a6b8ff02e3228a7afe900d6
zchanway <146023170+zchanway@users.noreply.github.com>
2024-10-12 04:09:27 -0500

    security

--- src/main/java/t_spring_security/evaluacion_service_security/security/SecurityConfig.java
+++ src/main/java/t_spring_security/evaluacion_service_security/security/SecurityConfig.java
@@ -1,9 +1,13 @@
 package t_spring_security.evaluacion_service_security.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import t_spring_security.evaluacion_service_security.service.DetalleUsuarioService;
 
 @EnableMethodSecurity
 @Configuration
@@ -11,4 +15,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @RequiredArgsConstructor
 
 public class SecurityConfig {
+    private final FiltroJWTAuth filtroJWTAuth;
+
+    private final DetalleUsuarioService detalleUsuarioService;
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(detalleUsuarioService).passwordEncoder(passwordEncoder());
+    }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }

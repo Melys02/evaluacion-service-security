a005ffad7909eed4d60b1cb28011f8c363c2025c
Melys02 <melys0919@gmail.com>
2024-10-12 03:38:14 -0500

    interface

--- src/main/java/t_spring_security/evaluacion_service_security/service/IUsuarioService.java
+++ src/main/java/t_spring_security/evaluacion_service_security/service/IUsuarioService.java
@@ -1,4 +1,11 @@
 package t_spring_security.evaluacion_service_security.service;
 
+import t_spring_security.evaluacion_service_security.model.Usuario;
+
+import java.util.Optional;
+
 public interface IUsuarioService {
+    Optional<Usuario> findByCodigo(String codigo);
+    Usuario registrarUsuario(String  codigo, String password, String email, Usuario.Rol rol);
+
 }

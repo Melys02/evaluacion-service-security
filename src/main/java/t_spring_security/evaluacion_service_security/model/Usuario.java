ba5da050abbd2506a4378cab28945200287f3cb7
Melys02 <melys0919@gmail.com>
2024-10-12 03:34:39 -0500

    entidad terminada

--- src/main/java/t_spring_security/evaluacion_service_security/model/Usuario.java
+++ src/main/java/t_spring_security/evaluacion_service_security/model/Usuario.java
@@ -1,4 +1,28 @@
 package t_spring_security.evaluacion_service_security.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "usuario")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String codigo;
+
+    private String password;
+
+    private String email;
+    private Rol rol;
+    private boolean activo;
+
+
+    public enum Rol {
+        COORDINADOR, GESTOR
+    }
 }

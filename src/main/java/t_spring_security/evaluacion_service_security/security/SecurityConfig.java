575201d04959ec0ba3d26cf106d3e6f80c7c8ce4
zchanway <146023170+zchanway@users.noreply.github.com>
2024-10-12 04:10:32 -0500

    bean

--- src/main/java/t_spring_security/evaluacion_service_security/security/SecurityConfig.java
+++ src/main/java/t_spring_security/evaluacion_service_security/security/SecurityConfig.java
@@ -3,7 +3,9 @@ package t_spring_security.evaluacion_service_security.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,5 +27,10 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
     }
 }
